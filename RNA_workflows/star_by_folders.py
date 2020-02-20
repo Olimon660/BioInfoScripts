977b9e44c3133610936f9ce4ddc58ca363b733da
scai <scai@cmri.org.au>
2020-02-20 16:31:27 +1100

    update

--- RNA_workflows/star_by_folders.py
+++ RNA_workflows/star_by_folders.py
@@ -23,12 +23,13 @@ if not os.path.isdir(output_root):
 
 def process(pair_dir):
     name = re.search(".*/(.*$)", pair_dir).group(1)
+    if os.path.exists(f"{output_root}/{name}_Aligned.out.bam"):
+        return
 
-    print(
-        (f'STAR --runThreadN 1 --genomeDir {genome_dir} --readFilesIn {pair_dir}/*gz '
-         f'--outFileNamePrefix {os.path.join(output_root, name)} '
+    os.system(
+        (f'STAR --runThreadN 4 --genomeDir {genome_dir} --readFilesIn {pair_dir}/*gz '
          f'--readFilesCommand zcat --outFileNamePrefix {output_root}/{name}_ --quantMode TranscriptomeSAM '
-         '--outSAMtype BAM'))
+         '--outSAMtype BAM Unsorted'))
 
 
-Parallel(n_jobs=1)(delayed(process)(subdirs[i]) for i in trange(len(subdirs)))
+Parallel(n_jobs=2)(delayed(process)(subdirs[i]) for i in trange(len(subdirs)))
